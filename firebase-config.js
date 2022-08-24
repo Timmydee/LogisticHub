7a0b707adf146258e85804b6e6c35201f63f3f3a
Timmydee <dahunsitimmy@gmail.com>
2022-08-24 12:05:28 -0700

    update

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAloXyfRITR3zHmW2CTYLCABdVGl2-KkE8",
+    authDomain: "logihub-93690.firebaseapp.com",
+    projectId: "logihub-93690",
+    storageBucket: "logihub-93690.appspot.com",
+    messagingSenderId: "141487837276",
+    appId: "1:141487837276:web:fcc6e1e3e2372949581699"
+};
