ea9eb061145cbd3dac7d9f05f260bd6fcce814a1
Timmydee <dahunsitimmy@gmail.com>
2022-09-01 22:11:23 -0700

    update

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 const navId = document.getElementById("nav-links")
 const toggleMenu = document.getElementById("toggle-btn")
 const closeMenu = document.getElementById("close-btn")
+const enterBtn = document.getElementById('enter')
+const paymentBar = document.getElementById('payment-col2')
+const line = document.getElementById('op-line')
 
 toggleMenu.addEventListener('click',() => {
     navId.style.right = "0"
@@ -10,31 +13,35 @@ closeMenu.addEventListener('click',() => {
     navId.style.right = "-100%"
 })
 
-// Animate on scroll
-AOS.init();
+enterBtn.addEventListener('click', function(){
+    paymentBar.style.display = "flex"
+    line.style.display = "block"
+})
+// // Animate on scroll
+// AOS.init();
 
-// GSAP ANIMATION
-gsap.from(".logo", {
-    opacity: 0,
-    y: -10,
-    delay: 1,
-    duration: 0.5,
-    stagger: 0.3,
-});
+// // GSAP ANIMATION
+// gsap.from(".logo", {
+//     opacity: 0,
+//     y: -10,
+//     delay: 1,
+//     duration: 0.5,
+//     stagger: 0.3,
+// });
 
-// TOGGLE BTN
-gsap.from(".toggle-btn",{
-    opacity: 0,
-    y: -10,
-    delay: 2.4,
-    duration: 1,
-});
+// // TOGGLE BTN
+// gsap.from(".toggle-btn",{
+//     opacity: 0,
+//     y: -10,
+//     delay: 2.4,
+//     duration: 1,
+// });
 
-// TEAM IMAGE
-gsap.from(".why-us-img",{
-    opacity: 0,
-    y: 20,
-    delay: 3,
-    duration: 10,
-});
+// // TEAM IMAGE
+// gsap.from(".why-us-img",{
+//     opacity: 0,
+//     y: 20,
+//     delay: 3,
+//     duration: 10,
+// });
 
