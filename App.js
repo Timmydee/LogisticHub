ecb617772837527eaca00e329f836af9be6ca78f
Timmydee <dahunsitimmy@gmail.com>
2022-09-09 16:32:40 -0700

    update

--- App.js
+++ App.js
@@ -17,31 +17,3 @@ enterBtn.addEventListener('click', function(){
     paymentBar.style.display = "flex"
     line.style.display = "block"
 })
-// // Animate on scroll
-// AOS.init();
-
-// // GSAP ANIMATION
-// gsap.from(".logo", {
-//     opacity: 0,
-//     y: -10,
-//     delay: 1,
-//     duration: 0.5,
-//     stagger: 0.3,
-// });
-
-// // TOGGLE BTN
-// gsap.from(".toggle-btn",{
-//     opacity: 0,
-//     y: -10,
-//     delay: 2.4,
-//     duration: 1,
-// });
-
-// // TEAM IMAGE
-// gsap.from(".why-us-img",{
-//     opacity: 0,
-//     y: 20,
-//     delay: 3,
-//     duration: 10,
-// });
-
